8cdbe6a51799822c340958951f1420c7bb4e9890
Pushon <asmpushon@gmail.com>
2016-07-31 17:55:37 +0600

    added 003_max_prime_factor.py

---
+++ 003_max_prime_factor.py
@@ -0,0 +1,47 @@
+
+# def isPrime(num):
+# 	is_prime = True
+# 	for i in xrange(2, int(num ** 0.5) + 2):
+# 		if num == 1:
+# 			is_prime = False
+# 			break
+# 		elif num == 2:
+# 			is_prime = True
+# 			break
+# 		else:
+# 			if num % i == 0:
+# 				is_prime = False
+# 				break
+
+# 	return is_prime
+
+
+# num = 600851475143	#Ans: 6857
+# max_prime_factor = 0
+
+# if isPrime(num):
+# 	max_prime_factor = num
+
+# else:
+# 	for i in xrange(1, int(num ** 0.5) + 2):
+# 		if num % i == 0:
+# 			if isPrime(i) and i > max_prime_factor:
+# 				max_prime_factor = i
+
+# 			j = num / i
+# 			if isPrime(j) and j > max_prime_factor:
+# 				max_prime_factor = j
+
+# print ("Maximum prime factor of %d is %d." % (num, max_prime_factor))
+
+
+#Nawrin's solution:
+n = 1024#600851475143
+for i in xrange(1, int(n/2) + 1):
+	if n % i == 0:
+		n = int(n / i)
+		print i
+		if n < i:
+			break
+
+# print n
